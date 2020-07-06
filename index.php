85eddc72f6164a911b55d7883de3e6f7756e5d04
AggieWB <usmaggie23@gmail.com>
2020-07-06 13:04:24 -0700

    Add link elements with css files and add title contents

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
-        <link href="css/style.css" rel="stylesheet">
+        <title>Aggie Wheeler Bateman</title>
+        <link href="css/portal.css" rel="stylesheet">
+        <link href="css/nav.css" rel="stylesheet">
+        <link href="css/form.css" rel="stylesheet">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
