5b17b1ee6045159a7df653317b76aae7ee5121fc
AggieWB <usmaggie23@gmail.com>
2020-07-06 13:25:46 -0700

    Add header section with wrapped ul in a nav element
    Add header inside of an main element
    Update title contents

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Aggie Wheeler Bateman</title>
+        <title>Aggie's IT162 Portal</title>
         <link href="css/portal.css" rel="stylesheet">
         <link href="css/nav.css" rel="stylesheet">
         <link href="css/form.css" rel="stylesheet">
@@ -10,12 +10,19 @@
         <meta name="robots" content="noindex, nofollow">
     </head>
     <body>
-        <ul>
-            <li><a href="index.php">Welcome</a></li>
-            <li><a href="big/index.php">Big</a></li>
-            <li><a href="fp/index.php">Final Project</a></li>
-            <li><a href="contactme.php">Contact Aggie</a></li>
-        </ul>
+        <main>
+            <header>
+                <h1><a href="index.php">Aggie Wheeler Bateman IT162 Portal</a></h1>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Welcome</a></li>
+                        <li><a href="big/index.php">Big</a></li>
+                        <li><a href="fp/index.php">Final Project</a></li>
+                        <li><a href="contactme.php">Contact Aggie</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </main>
         <script src=""></script>
     </body>
 </html>
