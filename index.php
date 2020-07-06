160b6bcc57779473871b4b109f8980484d909627
AggieWB <usmaggie23@gmail.com>
2020-07-06 13:50:13 -0700

    Add section element in the main element with three img
    elements and three p elements with content

--- index.php
+++ index.php
@@ -22,6 +22,20 @@
                     </ul>
                 </nav>
             </header>
+            <section>
+                <h2 class="welcome">Welcome!</h2>
+                <img alt=""></img>
+                <img alt=""></img>
+                <img alt=""></img>
+
+                <p>Hi! I'm Aggie, and I'm a junior web developer living in Seattle, Washington. I grew up in Mobile, Alabama where I loved spending time on the coast. I later attended the University of Southern Mississippi and earned my bachelor's degree in Elementary Education, but ultimately, I decided the career wasn't for me.</p>
+                
+                <p>I then enlisted in the Air Force and was trained extensively in medical laboratory science. After my training, I was stationed in Misawa, Japan where I worked for two years and earned my associate's degree in Medical Laboratory Technology. Once back in the states, I moved to Seattle and continued working as a medical laboratory scientist. </p> 
+            
+                <p>In the beginning of 2019, I was interested in programming and a possible career change, so I took the HTML, CSS, and JavaScript courses at <a href="https://www.freecodecamp.org/" target="_blank">freeCodeCamp</a>. I loved the problem solving and creativity that coding gave me, and I decided that I wanted to expand my knowledge and make the leap into changing careers. By September, I became a full-time web development student at Seattle Central College.</p>
+            
+                <p>Currently when I'm not studying programming, I enjoy spending time with my husband, reading, playing video games and board games, traveling, or working on personal programming projects. I aspire to learn as much as I can so I can put ideas into motion.</p>
+            </section>
         </main>
         <script src=""></script>
     </body>
