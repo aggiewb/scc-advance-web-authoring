1d7104c64c600c0e49ea78eb7c75d63daaaf1768
AggieWB <usmaggie23@gmail.com>
2020-07-10 09:50:58 -0700

    Update pipe symbol in link to hex code

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
         <link href="css/portal.css" rel="stylesheet">
         <link href="css/nav.css" rel="stylesheet">
         <link href="css/form.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Lilita+One|Open+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lilita+One%7COpen+Sans&display=swap" rel="stylesheet">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="noindex, nofollow">
