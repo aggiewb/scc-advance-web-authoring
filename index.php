0fe99bc238529ea1bdbbcad19af17e6620126f4d
AggieWB <usmaggie23@gmail.com>
2020-07-10 09:52:40 -0700

    Remove uneccesary closing tag for images

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
 <?php include "includes/header.php";?>
-        <!-- START LEFT COL -->
-        <main>
-            <div id="profile-pictures">
-                <img src="images/aggie_desktop.jpg" alt="Aggie standing in a courtyard in Germany" class="desktop"></img>
-                <img src="images/aggie_phone.png" alt="A headshot picture of Aggie with a wide smile" class="phone"></img>
-            </div>
-            <article>
-                <h2>About me</h2>
-                <p>My name is Aggie, and I am working towards a web development certificate from Seattle Central College with additional focus on web 
-                scripting and software development, and I currently live in the Maple Leaf neighborhood with my husband.</p>
+<main>
+    <div id="profile-pictures">
+        <img src="images/aggie_desktop.jpg" alt="Aggie standing in a courtyard in Germany" class="desktop">
+        <img src="images/aggie_phone.png" alt="A headshot picture of Aggie with a wide smile" class="phone">
+    </div>
+    <article>
+        <h2>About me</h2>
+        <p>My name is Aggie, and I am working towards a web development certificate from Seattle Central College with additional focus on web 
+        scripting and software development, and I currently live in the Maple Leaf neighborhood with my husband.</p>
+
+        <p>I grew up in Mobile, Alabama where I loved spending time on the coast. I later attended the University of Southern Mississippi and 
+        earned my bachelor's degree in Elementary Education, but ultimately, I decided the career wasn't for me.</p>
+        
+        <p>I then enlisted in the Air Force and was trained extensively in medical laboratory science. After my training, I was stationed in 
+        Misawa, Japan where I worked for two years and earned my associate's degree in Medical Laboratory Technology. Once back in the states, I 
+        moved to Seattle and continued working as a medical laboratory scientist.</p> 
     
-                <p>I grew up in Mobile, Alabama where I loved spending time on the coast. I later attended the University of Southern Mississippi and 
-                earned my bachelor's degree in Elementary Education, but ultimately, I decided the career wasn't for me.</p>
-                
-                <p>I then enlisted in the Air Force and was trained extensively in medical laboratory science. After my training, I was stationed in 
-                Misawa, Japan where I worked for two years and earned my associate's degree in Medical Laboratory Technology. Once back in the states, I 
-                moved to Seattle and continued working as a medical laboratory scientist.</p> 
-            
-                <p>In the beginning of 2019, I was interested in programming and a possible career change, so I took the HTML, CSS, and JavaScript courses 
-                at <a href="https://www.freecodecamp.org/" target="_blank">freeCodeCamp</a>. I loved the problem solving and creativity that coding gave 
-                me, and I decided that I wanted to expand my knowledge and make the leap into changing careers. By September, I became a full-time web 
-                development student.</p>
-            
-                <p>Currently when I'm not studying programming, I enjoy spending time with my husband, reading, playing video games and board games, traveling, 
-                or working on personal programming projects. I aspire to learn as much as I can so I can put ideas into motion.</p>
-            </article>
-            <aside>
-                <img src="images/aggie_tablet.jpg" class="tablet" alt="Aggie on a standing on a small bridge while on a nature hike">
-            </aside>
-        </main>
+        <p>In the beginning of 2019, I was interested in programming and a possible career change, so I took the HTML, CSS, and JavaScript courses 
+        at <a href="https://www.freecodecamp.org/" target="_blank">freeCodeCamp</a>. I loved the problem solving and creativity that coding gave 
+        me, and I decided that I wanted to expand my knowledge and make the leap into changing careers. By September, I became a full-time web 
+        development student.</p>
+    
+        <p>Currently when I'm not studying programming, I enjoy spending time with my husband, reading, playing video games and board games, traveling, 
+        or working on personal programming projects. I aspire to learn as much as I can so I can put ideas into motion.</p>
+    </article>
+    <aside>
+        <img src="images/aggie_tablet.jpg" class="tablet" alt="Aggie on a standing on a small bridge while on a nature hike">
+    </aside>
+</main>
 <?php include "includes/footer.php";?>
