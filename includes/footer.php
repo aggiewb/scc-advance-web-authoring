a54a7611c89e1920fac5a619b7d973b0f5fef186
AggieWB <usmaggie23@gmail.com>
2020-07-10 09:58:25 -0700

    Update year reference to php script for dynamic year

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
     <footer>
-        <p>&copy;2020 by <a href="contactme.php" target="_blank">Aggie Wheeler Bateman</a></p>
+        <p>&copy;<?php echo date("Y");?> by <a href="contactme.php" target="_blank">Aggie Wheeler Bateman</a></p>
         <div id="validators">
             <a href="http://validator.w3.org/check?uri=https://www.aggiewheelerbateman.com/it162/" target="_blank">Valid HTML</a>
             <a href="https://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a>
