48800ad8d864f4256a15ecdce0a908b56396800a
AggieWB <usmaggie23@gmail.com>
2020-07-10 09:51:51 -0700

    Update link for validators

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
-<footer>
-            <p>&copy;2020 by <a href="contactme.php" target="_blank">Aggie Wheeler Bateman</a></p>
-            <div id="validators">
-                <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a>
-                <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>
-            </div>
-        </footer>
-        <script src="js/nav.js"></script>
-    </body>
+    <footer>
+        <p>&copy;2020 by <a href="contactme.php" target="_blank">Aggie Wheeler Bateman</a></p>
+        <div id="validators">
+            <a href="http://validator.w3.org/check?uri=https://www.aggiewheelerbateman.com/it162/" target="_blank">Valid HTML</a>
+            <a href="https://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a>
+        </div>
+    </footer>
+    <script src="js/nav.js"></script>
+</body>
 </html>
