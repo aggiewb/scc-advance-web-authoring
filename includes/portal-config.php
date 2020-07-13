78db0b481731de6ec35ca9bb7c781109440a0dcf
AggieWB <usmaggie23@gmail.com>
2020-07-13 12:02:54 -0700

    Store function that returns the page name inside of a
    function

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -4,6 +4,8 @@ portal-config.php
 Used to store all of our WEB120 configuration information
 */
 
-echo basename($_SERVER['PHP_SELF']);
+
+define('THIS_PAGE', basename($_SERVER['PHP_SELF']);
+echo THIS_PAGE;
 die;
 ?>
