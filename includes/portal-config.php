0db9290c0fe0d41ecb3726ba21d715e42dab9817
AggieWB <usmaggie23@gmail.com>
2020-07-13 13:02:51 -0700

    Add function getTitleString and add switch for each web
    page with title and pageId references

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -4,15 +4,35 @@ portal-config.php
 Used to store all of our WEB120 configuration information
 */
 
-define('THIS_PAGE', basename($_SERVER['PHP_SELF']);
-THIS_PAGE;
+define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
+
+function getTitleString($pageType){
+    return 'Aggie\'s WEB120 ' . $pageType . ' Page';
+}
 
 switch(THIS_PAGE){
     case 'index.php':
-        $title = 'Aggie\'s WEB120 Title Page';
+        $title = getTitleString('Title');
+        $pageId = 'About me';
+    break;
+
+    case 'contactme.php':
+        $title = getTitleString('Contact');
+        $pageId = 'Contact Aggie';
+    break;
+
+    case 'flowchart.php':
+        $title = getTitleString('Flowchart');
+        $pageId = 'Flowchart';
+    break;
+
+    case 'aia.php':
+        $title = getTitleString('AIA');
+        $pageId = 'AIA';
     break;
 
     default:
         $title = THIS_PAGE;
+        $pageId = 'Doh';
 }
 ?>
