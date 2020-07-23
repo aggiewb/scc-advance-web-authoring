e6ef4b1cafcea7805ebfcb667e57cd0a2f5875a5
AggieWB <usmaggie23@gmail.com>
2020-07-23 15:11:05 -0700

    Add th and td content to first table and add h3 for table

--- aia.php
+++ aia.php
@@ -1,25 +1,26 @@
 <?php include "includes/header.php";?>
 <h2 class="pageId"><span class="fa <?=$logo?>"></span><?=$pageId?></h2>
+<h3>Client Information</h3>
 <table>
     <tr>
-        <th scope="row"></th>
-        <td></td>
+        <th scope="row">Client's Name</th>
+        <td>Steven Bateman</td>
     </tr>
     <tr>
-        <th scope="row"></th>
-        <td></td>
+        <th scope="row">Client's website name</th>
+        <td>Zevia’s RMMV Plugins</td>
     </tr>
     <tr>
-        <th scope="row"></th>
-        <td></td>
+        <th scope="row">Redesign or brand new website?</th>
+        <td>Brand new site</td>
     </tr>
     <tr>
-        <th scope="row"></th>
-        <td></td>
+        <th scope="row">Type of website</th>
+        <td>Community and Entertainment</td>
     </tr>
     <tr>
-        <th scope="row"></th>
-        <td></td>
+        <th scope="row">Client's goal(s)</th>
+        <td>The client's goal for their website is collating disparate sources of plugins in an easily searchable manner.</td>
     </tr>
 </table>
 
