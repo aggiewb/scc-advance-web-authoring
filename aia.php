884219ac9d923e1d59dcdc8a453ec2c465767ae6
AggieWB <usmaggie23@gmail.com>
2020-07-23 15:02:52 -0700

    Add two skeleton tables

--- aia.php
+++ aia.php
@@ -1,3 +1,48 @@
 <?php include "includes/header.php";?>
 <h2 class="pageId"><span class="fa <?=$logo?>"></span><?=$pageId?></h2>
+<table>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row"></th>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
 <?php include "includes/footer.php";?>
