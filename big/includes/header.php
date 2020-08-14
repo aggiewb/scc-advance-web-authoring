30f178cb16a7aa2babd286725c80dcc5109a642d
AggieWB <usmaggie23@gmail.com>
2020-08-14 12:31:19 -0700

    Change order of nav links

--- big/includes/header.php
+++ big/includes/header.php
@@ -17,8 +17,10 @@
                     <ul>
                         <li><a href="https://www.aggiewheelerbateman.com/it162/"><span><i class="fa fa-fw fa-bank"></i> IT162</span></a></li>
                         <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> BIG</span></a></li>
-                        <li><a href="flexbox.php"><span><i class="fa fa-fw fa-cube"></i> Flexbox</span></a></li>
-                        <li><a href="galleries.php"><span><i class="fa fa-fw fa-camera-retro"></i> Galleries</span></a></li>
+                        <li><a href="../research/flexbox.php"><span><i class="fa fa-fw fa-cube"></i> Flexbox</span></a></li>
+                        <li><a href="../research/galleries.php"><span><i class="fa fa-fw fa-camera-retro"></i> Galleries</span></a></li>
+                        <li><a href="../research/shopping-carts.php"><span><i class="fa fa-fw fa-shopping-basket"></i> Shopping Carts</span></a></li>
+                        <li><a href="../research/site-app.php"><span><i class="fa fa-fw fa-html5"></i> Site vs App</span></a></li>
                         <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
                         <ul>
                             <li><a href="calendar.php"><span><i class="fa fa-fw fa-calendar"></i> Calendar</span></a></li>
@@ -26,8 +28,6 @@
                             <li><a href="youtube.php"><span><i class="fa fa-fw fa-youtube-square"></i> YouTube</span></a></li>
                         </ul>
                         </li>
-                        <li><a href="shoppingcarts.php"><span><i class="fa fa-fw fa-shopping-basket"></i> Shopping Carts</span></a></li>
-                        <li><a href="siteapp.php"><span><i class="fa fa-fw fa-html5"></i> Site vs App</span></a></li>
                         <li><a href="webcam.php"><span><i class="fa fa-fw fa-eye"></i> Web Cam</span></a></li>
                     </ul>
                 </nav>
