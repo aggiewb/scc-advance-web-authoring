50eb26b1b6559e9afc6caad59547d62dc7d08ecb
AggieWB <usmaggie23@gmail.com>
2020-08-14 12:50:30 -0700

    Add research paper contents with resources to galleries.php

---
+++ big/research/galleries.php
@@ -0,0 +1,19 @@
+<?php include "includes/header.php";?>
+<h2>Galleries</h2>
+<p>Website media galleries can be a great way to show off relevant business products. There are a variety of options available to fit financial needs, feature requirements, and device support. Here we’ll discuss three responsive JavaScript based galleries, Nanogallery2, PhotoSwipe, and Galleria.</p>
+
+<p>Nanogallery2 is available under a proprietary commercial license which has two options. First, the website license for a single website costs $30 USD. Second, the OEM license for an unlimited number of websites or a single project, application, or theme costs $105 USD. These licenses help ensure that the tool is actively maintained and provides support for a limited time. The main features that this gallery includes are multiple gallery layouts, grid, justified, cascading, and mosaic, thumbnail hover and touch effects, and blurred image preview during image download. Most features are supported in Firefox 6+, Chrome 16+, Edge 13+, IE 11+, Opera 12.1+, Yandex 14.12+, iPhone 5+ / iOS 6+, and Safari 6.2+ (Brisbois).</p>
+
+<p>PhotoSwipe is free to use for commercial projects as it has been released under the MIT license. The main features that this gallery includes are use of all device touch gestures, unique URLs for each gallery item, and zooming of images on desktop and progressive loading of images as data arrives on desktop. The script is supported in desktop Chrome, Firefox, Safari, Opera and IE 8+. It provides mobile support in Android 2.3.7+, iOS 5+, Windows Phone 7+, BlackBerry OS 10+, and devices with multiple input methods such as Surface and Chromebook Pixel (Semenov).</p>
+
+<p>Galleria is also free to use for commercial projects as it has been released under the MIT license.The main features that this gallery includes are the availability to not only use images but also sources such as Flickr, Vimeo, and YouTube. Some other features are multiple themes for layout, the ability to create your own theme, and the use of device touch gestures. This script is supported in IE7+, Opera 11, Firefox 3+, Chrome 9+, Safari 4+ and Mobile Safari (Ready).</p>
+
+<p>Now equipped with the knowledge to make the best decision for your website needs, you’ll be able to catch a user’s attention with the magic of a JavaScript powered media gallery. The uses are seemingly endless and will provide an avenue for the visual imagination of your online presence to be brought to life.</p>
+
+<h3>Resources</h3>
+<ul>
+    <li>Brisbois, Christophe. “Modern Photo / Video Gallery and Lightbox [Javascript Library].” nanogallery2, 21 July 2020, <a href="nanogallery2.nanostudio.org/index.html" target="_blank">nanogallery2.nanostudio.org/index.html</a>.</li>
+    <li>“Ready to Put Galleria on Your Website?” Galleria, <a href="galleriajs.github.io/" target="_blank">galleriajs.github.io/</a>.</li>
+    <li>Semenov, Dmitry. “Responsive JavaScript Image Gallery.” PhotoSwipe, <a href="photoswipe.com/" target="_blank">photoswipe.com/</a>.</li>
+</ul>
+<?php include "includes/footer.php";?>
