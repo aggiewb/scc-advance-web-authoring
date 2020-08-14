41a53f81ab7b1130f6ae82b7845803506e006a46
AggieWB <usmaggie23@gmail.com>
2020-08-14 12:56:09 -0700

    Add research paper contents with resources to
    shopping-carts.php

---
+++ big/research/shopping-carts.php
@@ -0,0 +1,21 @@
+<?php include "includes/header.php";?>
+<h2>Shopping Carts</h2>
+<p>If you want to take your business into the digital age, an online shopping cart is a necessary component to monetizing your website. There are a variety of e-commerce software platforms available, and these platforms come in a variety of features and costs. Here we’ll look at three of PC Magazine’s top picks listed in 2019, Shopify, Wix Stores, and Ecwid (Martinez).</p>
+
+<p>Shopify offers support 24 hours a day 7 days a week, a free SSL certificate, multiple themes, and the ability to list unlimited products. They also offer a free two week trial with the lowest priced tier priced at $24 USD a month for hosting and two staff member accounts with admin and point of sale access. However, their purchase transaction fees could add up quickly, but may be due to their included fraud analysis (Shopify).</p>
+
+<p>Wix Stores lowest priced tier starts at $23 USD a month for hosting, support of point of sales systems, sales analytics and reports, and a free domain for a year. It also offers a full refund of its premium products within two weeks of purchase and no transaction fees on purchases. However, it only supports 20GB of storage (Wix).</p>
+
+<p>Ecwid offers a truly free online store with no time limit for 10 products, which is a great way to try out their service. Their shopping carts have responsive design so it looks great on desktop or mobile devices. The lowest priced tier starts at about $18 USD per month which includes chat support, GDPR compliance, 100 products, and 25GB per file of digital goods (Ecwid).</p>
+
+<p>All of these shopping cart platforms are scalable as your business grows to allow for more products, storage, and support. With this information and your own business analysis, you’ll be able to start down the right path to finding the right product to find your business needs.
+</p>
+
+<h3>Resources</h3>
+<ul>
+    <li>“Ecwid Pricing - Choose the Right Ecwid Plan for Your Business!” Ecwid, Ecwid, <a href="www.ecwid.com/pricing" target="_blank">www.ecwid.com/pricing</a>.</li>
+    <li>Martinez, Juan. “The Best E-Commerce Software.” PCMag, PCMag, 17 Sept. 2019, <a href="www.pcmag.com/picks/the-best-e-commerce-software" target="_blank">www.pcmag.com/picks/the-best-e-commerce-software</a>.</li>
+    <li>“Shopify Pricing - Setup and Open Your Online Store Today – Free Trial.” Shopify, Shopify, <a href="www.shopify.com/pricing" target="_blank">www.shopify.com/pricing</a>.</li>
+    <li>“Wix Pricing Information: Upgrade to a Premium Plan.” Wix Pricing Information | Upgrade to a Premium Plan | Wix.com, Wix, <a href="www.wix.com/upgrade/website" target="_blank">www.wix.com/upgrade/website</a>.</li>
+</ul>
+<?php include "includes/footer.php";?>
