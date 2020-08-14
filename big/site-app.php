536ce1af592d5206a804df0af16e56faeb1b7633
AggieWB <usmaggie23@gmail.com>
2020-08-14 14:18:57 -0700

    Add research php files into big directory

---
+++ big/site-app.php
@@ -0,0 +1,23 @@
+<?php include "/includes/header.php";?>
+<article>
+    <h2>Responsive vs Mobile</h2>
+
+    <p>When considering the options for your business’s online presence, you must evaluate whether a mobile app or a responsive website is more appropriate for your needs. They both have pros and cons, and we’ll examine both so you can make the best decision.</p>
+
+    <p>Since 81% of Americans own a smartphone and 52% own tablets as of 2019 (Demographics), mobile apps are an important option to consider. Mobile apps have access to specific features such as a camera, microphone, near-field communication, Face ID, and GPS. Users also spend more time while using and engaging with mobile apps (Cherednichenko). Native mobile apps make use of the efficiencies of specific devices to increase performance, consistency, and pleasant user experience. However, many people are experiencing “app fatigue” due to the amount of apps available, and it may be hard to acquire users (Mobile). You might also need to have multiple apps created for different systems, which will take a significant amount of time and money to develop.</p>
+
+    <p>Responsive websites are tailored for rendering on a variety of devices, such as mobile, tablet, and desktop, and a variety screen sizes (Responsive). They also take less time to develop than a mobile app and in turn may be cheaper than developing a mobile app. Google’s search engine also ranks responsive websites highest in their listings (Cherednichenko). However due to the varying kinds of platforms, browsers, and device specification, a responsive website may have unreliable performance and unusable features (Responsive).</p>
+
+    <p> Other aspects you may want to consider are future scalability to new features and customers, and whether you  need an offline mode. With this information and a comprehensive breakdown of the goals of your business website, such as a mobile market implementation versus acquiring clients, you should feel more equipped to make the decision between a responsive website and a mobile app.</p>
+</article>
+<aside>
+    <h3>Resources</h3>
+    <ul>
+        <li>Cherednichenko, Sveta. “Mobile App, Responsive Website, or Something in Between: What to Choose for Your Business.” Mobindustry, 20 May 2020, 
+        <a href="www.mobindustry.net/mobile-app-responsive-website-or-something-in-between-what-to-choose-for-your-business-in-2019/" target="_blank">www.mobindustry.net/mobile-app-responsive-website-or-something-in-between-what-to-choose-for-your-business-in-2019/</a>.</li>
+        <li>“Demographics of Mobile Device Ownership and Adoption in the United States.” Pew Research Center: Internet, Science & Tech, Pew Research Center, 5 June 2020, <a href="www.pewresearch.org/internet/fact-sheet/mobile/" target="_blank">www.pewresearch.org/internet/fact-sheet/mobile/</a>.</li>
+        <li>“Mobile App.” Wikipedia, Wikimedia Foundation, 23 July 2020, <a href="en.wikipedia.org/wiki/Mobile_app" target="_blank">en.wikipedia.org/wiki/Mobile_app</a>.</li>
+        <li>“Responsive Web Design.” Wikipedia, Wikimedia Foundation, 30 June 2020, <a href="en.wikipedia.org/wiki/Responsive_web_design" target="_blank">en.wikipedia.org/wiki/Responsive_web_design</a>.</li>
+    </ul> 
+</aside>
+<?php include "/includes/footer.php";?>
