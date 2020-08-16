236736e05b36738ec08c31f889b9a73e7afaf824
AggieWB <usmaggie23@gmail.com>
2020-08-16 14:46:45 -0700

    Add a iframe element of a Google map identifying the campus
    address
    Add an aside with a href of dialable telephone number and an address element

--- big/map.php
+++ big/map.php
@@ -1,5 +1,15 @@
 <?php include "./includes/header.php";?>
-<main>
+<main class="no-flex">
     <h2 class="pageId"><span class="fa <?=$logo?>"></span><?=$pageId?></h2>
+    <div class="display-flex">
+        <div class='embed-container'>
+            <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.509754517104!2d-122.32357374842566!3d47.616221579083245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906accdcfa1037%3A0x3612bda31a0c6106!2s1701%20Broadway%2C%20Seattle%2C%20WA%2098122!5e0!3m2!1sen!2sus!4v1597607457386!5m2!1sen!2sus' width='600' height='450' frameborder='0' style='border:0;' allowfullscreen='' aria-hidden='false' tabindex='0'></iframe>
+        </div>
+        <aside class="map-aside">
+            <h2>Contact Info</h2>
+            <address>1701 Broadway<br/>Seattle, WA 98122</address>
+            <a href="tel:12069343800">206.934.3800</a>
+        </aside>
+    </div>
 </main>
  <?php include "./includes/footer.php";?>
