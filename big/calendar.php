dc4bce8028aaeaff82968f4c1b46ae644bdbf06a
AggieWB <usmaggie23@gmail.com>
2020-08-17 11:14:50 -0700

    Add agenda-view responsive calendar

--- big/calendar.php
+++ big/calendar.php
@@ -2,9 +2,10 @@
 <main class="no-flex">
     <h2 class="pageId"><span class="fa <?=$logo?>"></span><?=$pageId?></h2>
     <div class='month-view embed-container'>
-        <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=MGFqdDU2cDlvcHA4czBpZTkxZTMzOXJ1aG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F6BF26&showTz=1' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe>
+        <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=MGFqdDU2cDlvcHA4czBpZTkxZTMzOXJ1aG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F6BF26&showTz=1&showTitle=0&mode=MONTH' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe>
     </div>
-
-
+    <div class='agenda-view embed-container'>
+        <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=MGFqdDU2cDlvcHA4czBpZTkxZTMzOXJ1aG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F6BF26&showTz=1&showTitle=0&mode=AGENDA' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe>
+    </div>
 </main>
  <?php include "./includes/footer.php";?>
