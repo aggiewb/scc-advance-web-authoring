c4d2e70e40216791f6b1871f5615c68d38007bd3
AggieWB <usmaggie23@gmail.com>
2020-08-17 10:39:42 -0700

    Add two responsive live webcams

--- big/webcam.php
+++ big/webcam.php
@@ -1,5 +1,19 @@
 <?php include "./includes/header.php";?>
-<main>
+<main class="no-flex">
     <h2 class="pageId"><span class="fa <?=$logo?>"></span><?=$pageId?></h2>
+        <div class="display-flex">
+            <section>
+                <h2>Brooks Falls - Katmai National Park, Alaska powered by EXPLORE.org</h2>
+                <div class='embed-container'>
+                    <iframe src='https://www.youtube.com/embed/IcWTPFnqOLo' frameborder='0' allowfullscreen></iframe>
+                </div>
+            </section>
+            <section>
+                <h2>WATCH NASA: Earth From Space - Earth Viewing cameras ISS feed</h2>
+                <div class='embed-container'>
+                    <iframe src='https://www.youtube.com/embed/DDU-rZs-Ic4' frameborder='0' allowfullscreen></iframe>
+                </div>
+            </section>
+        </div>
 </main>
  <?php include "./includes/footer.php";?>
